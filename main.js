39796021cb62060c5dc4d7e321c6f4d5f4d0116b
adepunandhu9@gmail.com <adepunandhu9@gmail.com>
2023-04-06 16:53:53 +0530

    jdjdjhdjhdd

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+// Loader
+const loader = document.querySelector('.loader-container');
+
 // First page elements 
 const firstPage = document.querySelector('.first-page');
 const newGame = document.querySelector('#new-game');
@@ -5,5 +8,14 @@ const scores = document.querySelector('#scores');
 
 // Second page elements
 const secondPage = document.querySelector('.second-page');
+
+newGame.addEventListener('click',() => {
+  firstPage.classList.remove('show');
+  loader.classList.add('show');
+  setTimeout(() => {
+    loader.classList.remove('show');
+    secondPage.classList.add('show');
+  },1000 * 3);
+});
 
 
