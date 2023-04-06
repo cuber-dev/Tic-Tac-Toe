a2c6ca3e78f372697bf911cf8bf99fd2f14be51f
adepunandhu9@gmail.com <adepunandhu9@gmail.com>
2023-04-06 17:41:36 +0530

    jdusujds

--- main.js
+++ main.js
@@ -5,17 +5,49 @@ const loader = document.querySelector('.loader-container');
 const firstPage = document.querySelector('.first-page');
 const newGame = document.querySelector('#new-game');
 const scores = document.querySelector('#scores');
+/* ======================================================= */
 
 // Second page elements
 const secondPage = document.querySelector('.second-page');
+const namesForm = document.querySelector('.second-page #name-form');
+const firstPlayerProfile = document.querySelector('#player-image-1');
+const firstPlayerImageInput = document.querySelector('#player-image-input-1');
+const firstPlayerImageLabel = document.querySelector('#player-image-label-1');
+const firstPlayerNameInput = document.querySelector('#player-name-input-1');
 
-newGame.addEventListener('click',() => {
+const secondPlayerProfile = document.querySelector('#player-image-2');
+const secondPlayerImageInput = document.querySelector('#player-image-input-2');
+const secondPlayerImageLabel = document.querySelector('#player-image-label-2');
+const secondPlayerNameInput = document.querySelector('#player-name-input-2');
+
+const formSubmitBtn = document.querySelector('#start-game');
+/* ======================================================= */
+function showSecondPage(){
   firstPage.classList.remove('show');
   loader.classList.add('show');
+  secondPage.classList.add('show');
   setTimeout(() => {
     loader.classList.remove('show');
-    secondPage.classList.add('show');
-  },1000 * 3);
+  }, 1000 * 3);
+}
+
+newGame.addEventListener('click',showSecondPage);
+
+namesForm.addEventListener('submit',(e) => {
+  e.preventDefault();
 });
 
+function loadImage(input,profile,label){
+  let imageSrc = input[0];
+  profile.classList.add('show');
+  label.classList.add('hide');
+  profile.src = URL.createObjectURL(imageSrc);
+}
 
+firstPlayerImageInput.addEventListener('change',() => {
+  loadImage(firstPlayerImageInput.files,firstPlayerProfile,firstPlayerImageLabel);
+});
+
+secondPlayerImageInput.addEventListener('change', () => {
+  loadImage(secondPlayerImageInput.files,secondPlayerProfile,secondPlayerImageLabel);
+});
