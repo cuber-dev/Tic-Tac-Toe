38e488fbf62647f2438edd7e7dbe4b47d82c0a42
adepunandhu9@gmail.com <adepunandhu9@gmail.com>
2023-04-06 16:30:12 +0530

    hshshhss

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('Hello World!');
+// First page elements 
+const firstPage = document.querySelector('.first-page');
+const newGame = document.querySelector('#new-game');
+const scores = document.querySelector('#scores');
+
+// Second page elements
+const secondPage = document.querySelector('.second-page');
+
+
